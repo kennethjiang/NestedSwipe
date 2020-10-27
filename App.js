4c010fb7adf59bc9d7261fad56ffd454202f6df7
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-10-27 07:13:15 -0700

    Bottom left/right swiper inside the up/down one. Does not seem to work

--- App.js
+++ App.js
@@ -212,6 +212,17 @@ const [pan, setPan] = useState(panResponder);
       >
         <Text>Pan Handler</Text>
       </View>
+      <Swiper style={styles.wrapper} showsButtons={false} pagingEnabled={false} loop={false}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
     </View>
   </View>;
 };
